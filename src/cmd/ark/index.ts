71924e963274457222c7b3016eae00046d06688a
Jonathan Hiles <jonathan@hil.es>
2025-01-17 19:16:26 +1000

    feat(ark): add `/ark listplayers` command

--- src/cmd/ark/index.ts
+++ src/cmd/ark/index.ts
@@ -2,12 +2,18 @@ import { type CommandContext, CommandOptionType, type SlashCreator } from "slash
 import type { Config } from "../../config";
 import { SlashCommand } from "../index.ts";
 import DinoWipeCommand from "./dinowipe.ts";
+import ListPlayersCommand from "./listplayers.ts";
 
 /**
  * A Discord command for interacting with an ARK: Survival Ascended cluster.
  */
 export default class ArkCommand extends SlashCommand {
   constructor(creator: SlashCreator, config: Config) {
+    const serverChoices = config.ark.servers.map((server) => ({
+      name: server.name,
+      value: server.name,
+      emoji: { name: server.emoji },
+    }));
     super(
       creator,
       {
@@ -20,6 +26,21 @@ export default class ArkCommand extends SlashCommand {
             name: "dinowipe",
             description: "Start a poll for an ARK dinosaur wipe.",
           },
+          // `/ark listplayers` - List online players on an ARK.
+          {
+            type: CommandOptionType.SUB_COMMAND,
+            name: "listplayers",
+            description: "List online players on an ARK.",
+            options: [
+              {
+                type: CommandOptionType.STRING,
+                name: "server",
+                description: "The ARK server to list online players.",
+                required: true,
+                choices: serverChoices,
+              },
+            ],
+          },
         ],
       },
       config,
@@ -30,6 +51,9 @@ export default class ArkCommand extends SlashCommand {
     switch (ctx.subcommands[0]!) {
       case "dinowipe":
         await new DinoWipeCommand(this, ctx, this.config).run(ctx);
+        break;
+      case "listplayers":
+        await new ListPlayersCommand(this, ctx, this.config).run(ctx);
         break;
     }
   }
